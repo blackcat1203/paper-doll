7c8226baa65d451ac58e49d1c7979777f78b3332
Leon <blackcat1203@gmail.com>
2020-03-17 19:50:20 +0800

    upload

--- paper-doll/ViewController.swift
+++ paper-doll/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headImage: UIImageView!
+    @IBOutlet weak var poseImage: UIImageView!
+    @IBOutlet weak var faceImage: UIImageView!
+    @IBOutlet weak var accessoriesImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func headButton(_ sender: UIButton) {
+        changeHead(to: sender.currentTitle!)
+    }
+    
+    @IBAction func poseButton(_ sender: UIButton) {
+        changePose(to: sender.currentTitle!)
+    }
+    
+    @IBAction func faceButton(_ sender: UIButton) {
+        changeFace(to: sender.currentTitle!)
+    }
+    
+    @IBAction func accessoriesButton(_ sender: UIButton) {
+        changeAccessories(to: sender.currentTitle!)
+    }
+    
+    func changeHead(to head:String){
+        headImage.image = UIImage(named:head)
+    }
+    
+    func changePose(to pose:String){
+        poseImage.image = UIImage(named:pose)
+    }
+    
+    func changeFace(to face:String){
+        faceImage.image = UIImage(named:face)
+    }
+    
+    func changeAccessories(to accessories:String){
+        accessoriesImage.image = UIImage(named:accessories)
+    }
 }
 
